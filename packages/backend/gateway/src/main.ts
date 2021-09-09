fafdaa7956144b4d2c16e4752e67c4b673d3cbc5
Cahllagerfeld <Cahllagerfeld>
2021-09-09 15:23:26 +0000

    fix: adjust gateway

--- packages/backend/gateway/src/main.ts
+++ packages/backend/gateway/src/main.ts
@@ -1,22 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://localhost:5672'],
-        queue: 'cats_queue',
-        queueOptions: {
-          durable: false,
-        },
-      },
-    },
-  );
-
-  app.listen();
+  async function bootstrap() {
+    const PORT = process.env.PORT || 3000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT);
+  }
+  bootstrap();
 }
 bootstrap();
