097e82ee39bd7d016dfa34438992f31069ef5cc4
Cahllagerfeld <Cahllagerfeld>
2021-09-18 11:51:36 +0000

    feat: add spotify auth

--- packages/backend/spotify-auth/src/app.controller.ts
+++ packages/backend/spotify-auth/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('loginWithCode')
+  async getNotifications(@Payload() code: string) {
+    const tokenResponse = await this.appService.getTokenFromCode(code);
+    const loggedInMail = await this.appService.getEmail(
+      tokenResponse.access_token,
+    );
+    return { ...tokenResponse, ...{ email: loggedInMail } };
   }
 }
--- packages/backend/spotify-auth/src/app.service.ts
+++ packages/backend/spotify-auth/src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import spotifyNode from 'spotify-web-api-node';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly config: ConfigService) {}
+
+  public async getTokenFromCode(code: string) {
+    const spotify = new spotifyNode({
+      clientId: this.config.get('SPOTIFY_CLIENT_ID'),
+      clientSecret: this.config.get('SPOTIFY_CLIENT_SECRET'),
+      redirectUri: this.config.get('SPOTIFY_REDIRECT_URI'),
+    });
+
+    const accessToken = await spotify.authorizationCodeGrant(code);
+    return { ...accessToken.body };
+  }
+
+  public async getEmail(accessToken: string) {
+    const spotify = new spotifyNode({
+      accessToken,
+    });
+
+    const profile = await spotify.getMe();
+
+    return profile.body.email;
   }
 }
